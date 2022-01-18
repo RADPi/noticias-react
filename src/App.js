71768d82e6f1940228f42f31db0ebd637ed9f1bf
Rafael Davila Pico <radp71@gmail.com>
2022-01-18 11:26:07 -0300

    Final

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import { useEffect, useState } from 'react'
 import Formulario from './components/Formulario'
 import Header from './components/Header'
 import ListadoNoticias from './components/ListadoNoticias'
